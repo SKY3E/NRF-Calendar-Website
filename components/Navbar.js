15605746ac7f968a22e6f0359a86e51ef2d7d548
SKY3E <bessin.raphael@gmail.com>
2023-03-26 19:03:39 -0400

    Quick & Auth Redirect Edits - 3/26/23

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 export default function Navbar() {
 
   return (
-    <nav class="border-b-4 bg-slate-800 p-1">
-      <img class="h-24" src={'/calendar.png'} />
-      <div></div>
+    <nav class="border-b-4 bg-slate-800 p-2">
+      <ul class="flex align-middle justify-between items-center">
+        <li><Link href="/"><img class="h-24" src={'/calendar.png'} /></Link></li>
+        <li><Link href="/enter"><button class="w-56 border-4 rounded p-2 mt-2 bg-white hover:bg-gray-100 text-gray-500">Enter Page</button></Link></li>
+      </ul>
     </nav>
   );
 }
