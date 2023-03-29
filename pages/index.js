0858f1d1c88e7854531be89e7dd2db27670ba662
SKY3E <bessin.raphael@gmail.com>
2023-03-28 20:58:49 -0400

    Index Reformating Edits - 3/28/23

--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,26 @@ export default function Home() {
   const { user, username } = useContext(UserContext);
 
   return (
-    <main class="mt-2 flex justify-center items-center">
-      {/* Check if user is signed in - if not, show sign in form, else show sign out button, else show sign in button */}
-      {user ? 
-        !username ? <UsernameForm /> : <SignOutButton />
-        : 
-        <SignInButton />
-      }
+    <main class="mt-2 flex flex-row justify-around items-center">
+      <div class="mt-2 flex flex-col justify-center items-center basis-2/3">
+        <section class="border-4 rounded-lg p-2 mb-2 bg-red-400 w-8/12">
+          <h4 class="text-4xl font-bold text-slate-800">Welcome to Open Calendar!</h4>
+          <p class="text-slate-800">Organize your day, week, or month with <strong>Open Calendar</strong>, <br></br> by signing up today or logging in.</p>
+        </section>
+        {/* Check if user is signed in - if not, show sign in form, else show sign out button, else show sign in button */}
+        {user ? 
+         !username ? <UsernameForm /> : <SignOutButton />
+         : 
+         <SignInButton />
+        }
+        <section class="border-4 rounded-lg p-2 mt-2 bg-red-400 w-8/12">
+         <h4 class="text-4xl font-bold text-slate-800">How to use ?</h4>
+         <p class="text-slate-800">Click on the + at the top of the screen to add an item to your <br></br> personalized calendar.</p>
+        </section>
+      </div>
+      <div class="basis-1/3">
+        <img class="h-96" src={'/calendar.png'} />
+      </div>
     </main>
   );
 }
